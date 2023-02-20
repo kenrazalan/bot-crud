0a0e4c30376b4e98466b80a197835e9eb76d0814
kenrazalan <kennethrazalan29@gmail.com>
2023-02-20 15:10:02 +0800

    add Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,63 @@
+import {
+  Header,
+  Container,
+  Group,
+  Burger,
+  Paper,
+  Transition,
+  Text,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { useState } from "react";
+import { HEADER_HEIGHT, useStyles } from "./styles";
+
+interface HeaderResponsiveProps {
+  links: { link: string; label: string }[];
+}
+export function HeaderComponent({ links }: HeaderResponsiveProps) {
+  const [opened, { toggle, close }] = useDisclosure(false);
+  const [active, setActive] = useState(links[0].link);
+  const { classes, cx } = useStyles();
+
+  const items = links.map((link) => (
+    <a
+      key={link.label}
+      href={link.link}
+      className={cx(classes.link, {
+        [classes.linkActive]: active === link.link,
+      })}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(link.link);
+        close();
+      }}
+    >
+      {link.label}
+    </a>
+  ));
+  return (
+    <Header height={HEADER_HEIGHT} mb={120} className={classes.root}>
+      <Container className={classes.header}>
+        <Text>Logo</Text>
+        <Group spacing={5} className={classes.links}>
+          {items}
+        </Group>
+
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          className={classes.burger}
+          size="sm"
+        />
+
+        <Transition transition="pop-top-right" duration={200} mounted={opened}>
+          {(styles) => (
+            <Paper className={classes.dropdown} withBorder style={styles}>
+              {items}
+            </Paper>
+          )}
+        </Transition>
+      </Container>
+    </Header>
+  );
+}
