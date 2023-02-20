b6e0b64bee17c6cc7b985794986b817acb50248e
kenrazalan <kennethrazalan29@gmail.com>
2023-02-20 14:59:47 +0800

    setup rroutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  redirect,
+  Navigate,
+} from "react-router-dom";
+import BotList from "./components/BotList";
+import { MantineProvider } from "@mantine/core";
+import { HeaderComponent } from "./components/Header/Header";
 
 function App() {
+  const links = [
+    {
+      link: "/",
+      label: "Bot List",
+    },
+    {
+      link: "/add",
+      label: "Add Bot",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">Initial</header>
-    </div>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <BrowserRouter>
+        <HeaderComponent links={links} />
+        <div>
+          <Routes>
+            <Route element={<BotList />} path="/" />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </MantineProvider>
   );
 }
 
---
+++ src/components/BotList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BotList() {
+  return <div>BotList</div>;
+}
+
+export default BotList;
