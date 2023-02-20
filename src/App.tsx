4e4e81dc0b4bcdf49cac798d6f4fa6ad78315646
kenrazalan <kennethrazalan29@gmail.com>
2023-02-20 15:23:13 +0800

    add bots context

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  redirect,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import BotList from "./components/BotList";
 import { MantineProvider } from "@mantine/core";
 import { HeaderComponent } from "./components/Header/Header";
 import useLocalStorage from "./hooks/useLocalStorage";
 import { links } from "./components/Header/styles";
+import BotsContext from "./context/BotsContext";
 
 function App() {
   const [bots, setBots] = useLocalStorage("bots", []);
@@ -17,13 +12,15 @@ function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <BrowserRouter>
-        <HeaderComponent links={links} />
-        <div>
-          <Routes>
-            <Route element={<BotList />} path="/" />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </div>
+        <BotsContext.Provider value={{ bots, setBots }}>
+          <HeaderComponent links={links} />
+          <div>
+            <Routes>
+              <Route element={<BotList />} path="/" />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </div>
+        </BotsContext.Provider>
       </BrowserRouter>
     </MantineProvider>
   );
---
+++ src/context/BotsContext.ts
@@ -0,0 +1,5 @@
+import React from "react";
+
+const BotsContext = React.createContext({});
+
+export default BotsContext;
